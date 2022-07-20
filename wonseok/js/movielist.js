4b8f2410b0270efe43c200b2bf3060c640168c34
doosan20582 <doosan2058@gmail.com>
2022-07-20 16:40:30 +0900

    07-20 16:40 최원석

--- wonseok/js/movielist.js
+++ wonseok/js/movielist.js
@@ -2,18 +2,23 @@ let genreSelect_C = document.getElementById('genreSelect_C');
 let yearSelect_C = document.getElementById('yearSelect_C');
 const btnSelect_C = document.getElementById('btnSelect_C');
 let movieContainer_C = document.getElementById('movieContainer_C');
+let pageBtnContainer_C = document.getElementById('pageBtnContainer_C');
 
 
 let genreSelectText;
 let yearSelectText;
-
+let sortedJsonArrayCount = 0;
+let pageStartCount = 0;
+let pageEndCount = 0;
 /*jsonArray 에서 정렬된 객체 배열*/
+let sortedJsonArray = [];
 
 function doAjax(){
+    console.clear();
     let xhr = new XMLHttpRequest();
     
     let jsonArray = [];
-    let sortedJsonArray = [];
+    sortedJsonArray = [];
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
             
@@ -23,7 +28,12 @@ function doAjax(){
             let obj = new Object();
             obj.genre = returnJson[i].genre.toLowerCase();
             obj.title = returnJson[i].title;
-            obj.open = returnJson[i].open;
+            // obj.open = returnJson[i].open;
+            let year_ = returnJson[i].open;
+            let year = year_.split('-',1);
+            // year = parseInt(year);
+            // console.log(year);
+            obj.open = year;
             obj.grade = returnJson[i].grade;
             obj.img = returnJson[i].img;
             obj.story = returnJson[i].story;
@@ -38,10 +48,15 @@ function doAjax(){
         // console.log(jsonArray);
        
         let divs = document.querySelectorAll('.movies');
+        let divs2 = document.querySelector('#divPage');
         for(let i = 0; i < divs.length; i++){
-            console.log('지우는중');
+            // console.log('지우는중');
             divs[i].remove();
         }
+        if(divs2)
+            divs2.remove();
+        
+        
     
         if(genreSelectText == 'all'){
             if(yearSelectText == 'all'){
@@ -413,9 +428,9 @@ function doAjax(){
             else
                 alert('에러');
         }
-        else if(genreSelectText == 'animated'){
+        else if(genreSelectText == 'animation'){
             
-            let pattern = /animated/g;
+            let pattern = /animation/g;
             
             if(yearSelectText == 'all'){
                 for(let i = 0; i < jsonArray.length; i++){
@@ -611,30 +626,138 @@ function doAjax(){
         drawArray();
 
         function drawArray(){
-            for(let i = 0; i < sortedJsonArray.length; i++){
+            console.log(sortedJsonArray);
+            if(sortedJsonArray.length > 10){
+                pageStartCount = 0;
+                pageEndCount = 10;
+                pageBtnContainer_C.style.display = 'block';
+                
+                if(sortedJsonArray.length % 10 == 0){
+                    sortedJsonArrayCount = sortedJsonArray.length / 10;
+                }
+                else{
+                    sortedJsonArrayCount = Math.floor(sortedJsonArray.length / 10) + 1;
+                }
+                
+                console.log(`정렬된 영화 수: ${sortedJsonArray.length}`);
+                console.log(`페이지 수: ${sortedJsonArrayCount}`);
+                
+                let divPage = document.createElement('div');
+                pageBtnContainer_C.appendChild(divPage);
+                divPage.id = 'divPage';
+                
+                let aPrev = document.createElement('a');
+                aPrev.textContent = '이전';
+                divPage.appendChild(aPrev);
+                
+                for(let j = 0; j < sortedJsonArrayCount; j++ ){
+                    let aPage = document.createElement('a');
+                    aPage.textContent = `${j + 1}`;
+                    divPage.appendChild(aPage);
+
+                    aPage.addEventListener('click' , pageShow );
+                }
+                
+                let aNext = document.createElement('a');
+                aNext.textContent = '다음';
+                divPage.appendChild(aNext);
+            }
+            
+            else{
+                pageStartCount = 0;
+                pageEndCount = sortedJsonArray.length;
+                pageBtnContainer_C.style.display = 'none';
+                
+                console.log(`정렬된 영화 수: ${sortedJsonArray.length}`);
+            }
+            
+            for(let i = pageStartCount; i < pageEndCount; i++){
 
                 let div = document.createElement('div');
+                let poster = document.createElement('img');
                 let h1 = document.createElement('h1');
                 let h2 = document.createElement('h2');
                 let h3 = document.createElement('h3');
                 
                 div.className = `movies${i} movies`;
-               
+                poster.className = 'poster';
+                
                 h1.innerHTML = sortedJsonArray[i].title;
                 h2.innerHTML = sortedJsonArray[i].genre;
                 h3.innerHTML = sortedJsonArray[i].open;
+                poster.src = "./img/" + sortedJsonArray[i].img;
                 
                 movieContainer_C.appendChild(div);
+                document.querySelector(`.movies${i}`).appendChild(poster);
                 document.querySelector(`.movies${i}`).appendChild(h1);
                 document.querySelector(`.movies${i}`).appendChild(h2);
                 document.querySelector(`.movies${i}`).appendChild(h3);
+                
             }
+
+            
         }
 
         }
     }
 
-    xhr.open('get','./json/movielist.json');
+    xhr.open('get','./json/movielist2.json');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send();
+}
+
+function pageShow(){
+    let text = parseInt(this.textContent);
+    
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            
+            let divs = document.querySelectorAll('.movies');
+            for(let i = 0; i < divs.length; i++){
+                divs[i].remove();
+            }
+
+            if(text != sortedJsonArrayCount){
+                pageStartCount = (text * 10) - 10;
+                pageEndCount = text * 10;
+                console.log(pageStartCount);
+                console.log(pageEndCount);
+            }
+            else{
+                pageStartCount = (text * 10) - 10;
+                pageEndCount = sortedJsonArray.length;
+            }
+
+            for(let i = pageStartCount; i < pageEndCount; i++){
+
+                let div = document.createElement('div');
+                let poster = document.createElement('img');
+                let h1 = document.createElement('h1');
+                let h2 = document.createElement('h2');
+                let h3 = document.createElement('h3');
+                
+                div.className = `movies${i} movies`;
+                poster.className = 'poster';
+                
+                h1.innerHTML = sortedJsonArray[i].title;
+                h2.innerHTML = sortedJsonArray[i].genre;
+                h3.innerHTML = sortedJsonArray[i].open;
+                poster.src = "./img/" + sortedJsonArray[i].img;
+                
+                movieContainer_C.appendChild(div);
+                document.querySelector(`.movies${i}`).appendChild(poster);
+                document.querySelector(`.movies${i}`).appendChild(h1);
+                document.querySelector(`.movies${i}`).appendChild(h2);
+                document.querySelector(`.movies${i}`).appendChild(h3);
+                
+            }
+        }
+    }
+   
+
+    xhr.open('get','./json/movielist2.json');
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send();
 }
