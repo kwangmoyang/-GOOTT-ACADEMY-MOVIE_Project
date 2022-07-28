eaed67d82e8a5678f932801742bcd0edacb119e6
kwangmoyang <zjqtm45@naver.com>
2022-07-28 15:37:33 +0900

    07-28 15:37 양광모

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ let fourTop = section[3].offsetTop;
 let menu = document.querySelector('.header_Y');
 let scrollTop = window.scrollY;
 let menu2 = document.querySelector('.header');
+
 // 라이트바 애니메이션
 let right = document.querySelector('#rightBar_Y').children;
 console.log(right[0]);
@@ -20,39 +21,40 @@ console.log(right[0]);
 let flag = false;
 window.addEventListener('scroll', function () {
     scrollTop = window.scrollY;
-    
+    // 스크롤 Y축이 6이상 넘어갈 경우 백그라운드 컬러 표츌
     if (scrollTop > 6) {
         menu.style.background = 'linear-gradient(to bottom, rgba(41, 40, 38, 1), rgba(41,40,38, 0))';
-        // menu.style.background = 'rgba(41, 40, 38, 0.7)';
-
+    // 6안쪽으로 들어왔을때 백그라운드 투명
     } else {
         menu.style.background = 'transparent';
-        menu.style.transition = 'all 1s linear 0s';
-
     }
 });
 
 
-
+// 라이트바 1번 박스 클릭 이벤트
 right[0].addEventListener('click',function(){
     window.scroll({ top: firstTop, behavior: 'smooth' });
     color(0,1,2,3);
 })
+// 라이트바 2번 박스 클릭 이벤트
 right[1].addEventListener('click',function(){
     window.scroll({ top: secondTop, behavior: 'smooth' });
     color(1,0,2,3);
 })
+// 라이트바 3번 박스 클릭 이벤트
 right[2].addEventListener('click',function(){
     window.scroll({ top: thirdTop, behavior: 'smooth' });
     color(2,0,1,3);
 })
+// 라이트바 4번 박스 클릭 이벤트
 right[3].addEventListener('click',function(){
     window.scroll({ top: fourTop, behavior: 'smooth' });
     color(3,0,1,2);
 })
+
 //윈도우 리사이즈 되면 섹션들 위치 다시 계산
 window.addEventListener('resize' , function(){
-
+    //최상위 객체 Y축 호출
     scrollTop = window.scrollY;
 
     firstTop = section[0].offsetTop;
@@ -62,10 +64,15 @@ window.addEventListener('resize' , function(){
     console.log(firstTop , secondTop , thirdTop , fourTop);  
 })
 
+// 메인페이지 각 섹션 화살표 버튼
+
+// 1번 섹션 클릭시 2번 섹션으로 이동
 scrollBtn[0].onclick = function () {
     window.scroll({ top: secondTop, behavior: 'smooth' });
+    // 1번 라이트 박스 색상 변경
     color(1,0,2,3);
 }
+
 scrollBtn[1].onclick = function () {
     window.scroll({ top: thirdTop, behavior: 'smooth' });
     color(2,0,1,3);
@@ -82,6 +89,7 @@ scrollBtn[3].onclick = function () {
 
 
 // right bar BOX 이펙트 함수
+// effect에 들어가는 인자값이 이벤트 발생시 효과 발생
 function color(effect,ele,ele2,ele3){
     right[effect].style.background='var(--bg-color)';
     right[effect].style.transition='.5s';
